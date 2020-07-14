eff9b4634f39d280bb6e42e8637aa8c9eb04d10b
Jaquelin Rocha <jaquir16@gmail.com>
2020-07-13 21:54:07 -0700

    created read me file

--- README.md
+++ README.md
@@ -1,1 +1,34 @@
-# readMe-Generator
+
+# EXPRESS README GENERATOR
+      
+ ## Description:
+    With this generator creating a readME file becomes a simple task, just provide answers to the prompted questions and a file will be created for you.   
+# Table of Contents
+* [Installation] 
+* [Usage] 
+* [Contributions]
+* [Test]
+* [License]
+* [Questions] 
+
+
+
+## Installation:
+    this application requires node.js, fs, util, inquirer, make sure these are installed
+## Usage:
+    to generate a README file for the user
+## Tests:
+    none
+## Contributions: 
+     UCR Bootcamp starter code
+## License:
+- 
+![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+
+    
+## Questions:
+    If you have any further questions please refer to the information below:
+* [GitHub](https://github.com/jackier94)
+
+* Email: jaquir16@gmail.com)
--- index.js
+++ index.js
@@ -7,70 +7,118 @@ const util = require("util");
 const writeFileAsync = util.promisify(fs.writeFile);
 
 // array of questions for user
-const questions = [];
+// const questions = [];
 
 //
 
 function promptUser() {
-    return inquirer.prompt([
-        {
-            type: "input",
-            message:"What is the title for your ReadMe?",
-            name: "title"
-        },
-        {
-            type:"input",
-            message: "please enter a description for your project",
-            name:"description"
-        },
-        {
-            type:"input",
-            message: "Do you have any instructions for installation? What are they? if no type NO.",
-            name:"instructions"
-        },
-        {
-            type:"input",
-            message: "what is the usage of your project?",
-            name: "usage"
-        },
-        {
-            type: "input",
-            message: "Do you have any contributions to add? if yes please write them in here",
-            name:"contributions"
-        },
-        {
-            type:"input",
-            message: "what are your test instructions?",
-            name:" Testing"
-        },
-        {
-            type:"checkbox",
-            message:"choose license type",
-            choices: [
-                "MIT License",
-                "GNU (GPL) 2.0",
-                "Apache License 2.0",
-                "GNU (GPL) 3.0"
-            ],
-            name: "license"
-        },
-        {
-            type:"input",
-            name: "username",
-            message:"What is your GitHub username?"
-        },
-        {
-            type:"input",
-            name:"email",
-            message:"provide your email address"
-        }
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is the title for your ReadMe?",
+      name: "title",
+    },
+    {
+      type: "input",
+      message: "please enter a description for your project",
+      name: "description",
+    },
+    {
+      type: "input",
+      message:
+        "Do you have any instructions for installation? What are they? if no type NO.",
+      name: "instructions",
+    },
+    {
+      type: "input",
+      message: "what is the usage of your project?",
+      name: "usage",
+    },
+    {
+      type: "input",
+      message:
+        "Do you have any contributions to add? if yes please write them in here",
+      name: "contributions",
+    },
+    {
+      type: "input",
+      message: "what are your test instructions?",
+      name: "testing",
+    },
+    {
+      type: "checkbox",
+      message: "choose license type",
+      choices: [
+        "MIT License",
+        "GNU (GPL) 2.0",
+        "Apache License 2.0",
+        "GNU (GPL) 3.0",
+      ],
+      name: "license",
+    },
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "provide your email address",
+    },
+  ]);
 }
 
 // function to write README file
-function writeToFile(fileName, data) {}
+function writetoFile(answers) {
+  return `
+# ${answers.title}
+      
+ ## Description:
+    ${answers.description}   
+# Table of Contents
+* [Installation] 
+* [Usage] 
+* [Contributions]
+* [Test]
+* [License]
+* [Questions] 
+
+
+
+## Installation:
+    ${answers.instructions}
+## Usage:
+    ${answers.usage}
+## Tests:
+    ${answers.testing}
+## Contributions: 
+     ${answers.contributions}
+## License:
+- 
+![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+
+    
+## Questions:
+    If you have any further questions please refer to the information below:
+* [GitHub](https://github.com/${answers.username})
+
+* Email: ${answers.email})
+`;
+}
 
 // function to initialize program
-function init() {}
+async function init() {
+  const answers = await promptUser();
+
+  const readMe = writetoFile(answers);
+
+  fs.writeFile("README.md", readMe, function (err) {
+    if (err) throw err;
+    console.log("Saved!");
+  });
+}
 
 // function call to initialize program
 init();
